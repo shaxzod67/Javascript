19e14835a96a4487ef523c3123cfe9d51fcb68c6
shaxzod67 <uzbuz741@gmail.com>
2024-02-17 14:58:31 +0500

    js

--- 7-dars/script.js
+++ 7-dars/script.js
@@ -1,18 +1,47 @@
+// let a = +prompt(' a son kiriting')
+// let b = +prompt(' b son kiriting')
+// let c = +prompt(' c son kiriting')
+
+// if ((a > b && a < c) || (a < b && a > c)) {
+// 	console.log(a + ' a son chiqdi')
+// } else if ((b > a && b < c) || (b < a && b > c)) {
+// 	console.log(b + ' b son chiqdi')
+// } else if ((c > a && c < b) || (c < a && c > b)) {
+// 	console.log(c + ' c son chiqdi')
+// } else if (a === b && b === c && c === a) {
+// 	console.log('Hamma son teng')
+// } else {
+// 	console.log('ikki sonni teng qilib quygansiz')
+// }
+
 //  1 - savol
 // let sum = 0
 // for (let i = 1; i <= 10; i++) {
 // 	sum += i
 // }
-// console.log(sum);
+// console.log(sum.repeat(i));
 
 // sum = sum + i
 // sum += i
 // 0 + i
 
-// 2 - savol
-let sum = +prompt('son kiriting ! ')
-let nom = prompt('matn kiriting ! ')
-for (let i = 0; i <= sum; i++) {
-	// sum = sum + i
-	console.log(i + ' ' + nom.repeat + `\n`)
-}
+// 2 - savol yulduzcha
+// let sum = +prompt('son kiriting ! ')
+// let nom = "*"
+// for (let i = 0; i <= sum; i++) {
+// 	console.log(' ' + nom.repeat(i))
+// }
+
+
+
+
+
+// let son = +prompt("Yulduzcha sonini kiriting");
+// let y = "*" 
+// for(let i = 1; i <= son; i++){
+// 	console.log(y.repeat(i));
+// }
+
+
+
+
