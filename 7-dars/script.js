b6660a15cceb0398aa9700be60db40186aad9957
shaxzod67 <uzbuz741@gmail.com>
2024-02-17 13:37:19 +0500

    JS

--- 7-dars/script.js
+++ 7-dars/script.js
@@ -1,3 +1,17 @@
-for (let i = 0; i <= 5; i++) {
-	console.log(i + 5 , i++)
+//  1 - savol
+// let sum = 0
+// for (let i = 1; i <= 10; i++) {
+// 	sum += i
+// }
+// console.log(sum);
+
+// sum = sum + i
+// sum += i
+// 0 + i
+
+// 2 - savol
+let sum = 1
+for (let i = 0; i <= 10; i++) {
+	sum = sum + i
+	console.log(sum)
 }
