ab47cc327ed631cd6eddbe8eea8f9d5ad1c48e1c
shaxzod67 <uzbuz741@gmail.com>
2024-02-21 19:15:37 +0500

    sardor

---
+++ 8-dars uyga vazifa/app.js
@@ -0,0 +1,45 @@
+// let button = document.getElementById('button')
+// let pp = document.getElementById('pp')
+
+// button.onclick = function hisob() {
+// 	let input = document.getElementById('input').value
+// 	let a = input
+// 	if (a < 18) {
+// 		pp.innerHTML = 'Siz balogat yoshida emassiz'
+// 	}
+// 	else if ( a >= 18 && a < 60 ){
+// 		pp.innerHTML = "Siz ishlaydigan yoshdasiz"
+// 	}
+// 	else if ( a >= 60){
+// 		pp.innerHTML = "Siz nafaqadasiz"
+// 	}
+// }
+
+// 1- vazifa
+
+// let input = document.getElementById('input')
+// let parol = document.getElementById('parol')
+// let button = document.getElementById('button')
+// let pp = document.getElementById('pp')
+
+// function hisob() {
+// 	let input = document.getElementById('input').value
+// 	let parol = document.getElementById('parol').value
+// 	let surname = input
+// 	if (surname == 'olimjonov') {
+// 		if (parol == 12345) {
+// 			// pp.innerHTML = ' '
+// 			window.open('https://olimjonovdev.vercel.app/')
+// 		} else {
+// 			pp.innerHTML = 'Siz login yoki passwordni  xato kiritdingiz'
+// 		}
+// 	} else {
+// 		pp.innerHTML = ' Siz login yoki passwordni  xato kiritdingiz'
+// 	}
+// }
+// button.onclick = hisob
+
+//  2- vazifa
+
+
+
---
+++ 9-dars/app.js
@@ -0,0 +1,62 @@
+// let button = document.getElementById('button')
+// let pp = document.getElementById('pp')
+
+// function hisob() {
+// 	let input = +document.getElementById('input').value
+// 	let input1 = +document.getElementById('input1').value
+// 	let sel = +document.getElementById('sel').value
+// 	let bir = document.getElementById('bir').value
+// 	let ikki = document.getElementById('ikki').value
+// 	let uch = document.getElementById('uch').value
+// 	let tort = document.getElementById('tort').value
+
+// 	if (sel == bir) {
+// 		pp.innerHTML = input + input1
+// 	} else if (sel == ikki) {
+// 		pp.innerHTML = input - input1
+// 	} else if (sel == uch) {
+// 		pp.innerHTML = input * input1
+// 	} else if (sel == tort) {
+// 		pp.innerHTML = input / input1
+// 	}
+	
+// }
+
+// button.onclick = hisob
+
+// let button = document.getElementById('button')
+// let pp = document.getElementById('pp')
+
+// function hisob() {
+// 	let a = document.getElementById('bir').value
+// 	let b = document.getElementById('ikki').value
+// 	let c = document.getElementById('uch').value
+
+// 	for( a > b && a > c) {
+// 		pp.innerHTML = a + " Bu katta son"	}
+// }
+// button.onclick = hisob
+
+// let kun = +prompt('Son')
+// switch (kun) {
+// 	case 1:
+// 	case 2:
+// 	case 3:
+// 		document.write('Qish')
+// 		break
+// 	case 4:
+// 	case 5:
+// 	case 6:
+// 		document.write('Bahor')
+// 		break
+// 	case 7:
+// 	case 8:
+// 	case 9:
+// 		document.write('Yoz')
+// 		break
+// 	case 10:
+// 	case 11:
+// 	case 12:
+// 		document.write('Kuz')
+// 		break
+// }
