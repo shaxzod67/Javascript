9c5a562d4dae05c97dbe6173e793a65a71bee7fd
Your Name <you@example.com>
2024-02-23 20:15:47 +0500

    js function juda qiyin boldi organish lekin uddaladim

---
+++ 10-dars/script.js
@@ -0,0 +1,70 @@
+
+// let bir = +prompt("son 1")
+// let ikki = +prompt("son 2")
+
+
+
+// function myFc( a  , b ) {
+//     console.log(a + b);
+// }
+// myFc(bir , ikki);
+
+
+
+
+// let ism = prompt( " ismni kiriting")
+// let familya = prompt( " familyani kiriting")
+
+
+// function ot (z,sh){
+//     return console.log("Assalomu aleykum " + z + sh) ;
+// }
+// ot(ism,familya)
+
+
+
+
+// let a = +prompt("yosh")
+
+// function hisob (qiymat){
+//     if( a > 12){
+//         return console.log("Siz tzizmga kirdingiz");
+//     } 
+//     else{
+//         console.log("siz hali yoshga tolmagansiz");
+//     }
+// }
+// hisob(a)
+
+
+
+// let bir = +prompt("son 1")
+// let m = prompt( " amal kiriting")
+// let ikki = +prompt("son 2")
+
+// function hisob( a , b){
+//     return a + b;
+// }
+// function minus( a ,b){
+//     return a - b;
+// }
+// function kop( a ,b){
+//     return a * b;
+// }
+// function bul( a ,b){
+//     return a / b;
+// }
+// switch (m) {
+//     case "+":
+//         console.log(hisob(bir,ikki));
+//         break;
+//       case "-":
+//         console.log(minus(bir,ikki));
+//         break;
+//         case "*":
+//         console.log(kop(bir,ikki));
+//         break;
+//         case "/":
+//         console.log(bul(bir,ikki));
+//         break;
+// }
