256fe54578b73cba981d91aa90c0f416d56e2204
shaxzod67 <uzbuz741@gmail.com>
2024-02-23 11:37:22 +0500

    JS my live

---
+++ 9-dars vazifa/app.js
@@ -0,0 +1,40 @@
+// let input1 = document.getElementById('input1')
+// let input2 = document.getElementById('input2')
+// let input3 = document.getElementById('input3')
+// let button = document.getElementById('button')
+// let pp = document.getElementById('pp')
+
+// function hisob() {
+// 	let input1 = +document.getElementById('input1').value
+// 	let input2 = +document.getElementById('input2').value
+// 	let input3 = +document.getElementById('input3').value
+// 	let a = input1
+// 	let b = input2
+// 	let c = input3
+// 	if (a == b && b == c && c == a) {
+// 		pp.innerHTML = ' Bu sonlar teng'
+// 	} else if (a > b && a > c) {
+// 		pp.innerHTML = a + ' Bu eng katta son'
+// 	} else if (b > a && b > c) {
+// 		pp.innerHTML = b + ' Bu eng katta son'
+// 	} else if (c > a && c > b) {
+// 		pp.innerHTML = c + ' Bu eng katta son'
+// 	} else if (b == a && c < a) {
+// 		pp.innerHTML = b + ' va ' + a + '  eng katta son'
+// 	} else if (a == c && b < c) {
+// 		pp.innerHTML = c + ' va ' + a + '  eng katta son'
+// 	} else if (b == c && a < c) {
+// 		pp.innerHTML = c + ' va ' + b + '  eng katta son'
+// 	} else {
+// 		;(" Siz bo'sh qiymat kiritdengiz")
+// 	}
+// }
+// button.onclick = hisob
+
+
+
+let input1 = document.getElementById('input1')
+let input2 = document.getElementById('input2')
+let input3 = document.getElementById('input3')
+let button = document.getElementById('button')
+let pp = document.getElementById('pp')
