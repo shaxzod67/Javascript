9593a0256d9fb41aae44e3022fedcc36c506a32d
Your Name <you@example.com>
2024-02-23 12:44:10 +0500

    new

--- 9-dars vazifa/app.js
+++ 9-dars vazifa/app.js
@@ -33,8 +33,35 @@
 
 
 
-let input1 = document.getElementById('input1')
-let input2 = document.getElementById('input2')
-let input3 = document.getElementById('input3')
+
 let button = document.getElementById('button')
 let pp = document.getElementById('pp')
+
+
+function hisob() {
+    let input1 = document.getElementById('input1').value
+    let input2 = document.getElementById('input2').value
+    let input3 = document.getElementById('input3').value
+    let a = input1
+    let b = input2
+    let c = input3
+    let d = "  soni eng kichigi"
+
+    if( a == b && b == c && c == a){
+        pp.innerHTML = " Bu sonlar o'zaro teng"
+    }
+    else if ( a < b && a < c){
+        pp.innerHTML = a +  d
+    }
+    else if (b < a && b < c ){
+        pp.innerHTML = b + d
+    }
+    else if ( c < a && c < b){
+        pp.innerHTML = c + d
+    }
+    else if ( a == b && b < c || a == b && b > c )
+    pp.innerHTML = a + " va " + b +" tang" + c 
+
+
+}
+button.onclick = hisob
