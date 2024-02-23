9ef6b7573923c8d46d77d7c42702c30bec2077dc
Your Name <you@example.com>
2024-02-23 15:28:00 +0500

    js

--- 9-dars vazifa/app.js
+++ 9-dars vazifa/app.js
@@ -30,38 +30,82 @@
 // 	}
 // }
 // button.onclick = hisob
+////////////////////////////////////////////////////////////////////// katta son
+
+
+
+// let button = document.getElementById('button')
+// let pp = document.getElementById('pp')
+
+
+// function hisob() {
+//     let input1 = document.getElementById('input1').value
+//     let input2 = document.getElementById('input2').value
+//     let input3 = document.getElementById('input3').value
+//     let a = input1
+//     let b = input2
+//     let c = input3
+//     let d = "  soni eng kichigi"
+
+//     if( a == b && b == c && c == a){
+//         pp.innerHTML = " Bu sonlar o'zaro teng"
+//     }
+//     else if ( a < b && a < c){
+//         pp.innerHTML = a +  d
+//     }
+//     else if (b < a && b < c ){
+//         pp.innerHTML = b + d
+//     }
+//     else if ( c < a && c < b){
+//         pp.innerHTML = c + d
+//     }
+//     else if ( a == b &&  b < c  ){
+//         pp.innerHTML = a + " va " + b + d
+//     }
+//     else if ( a == c &&  c < b  ){
+//         pp.innerHTML = a + " va " + c + d
+//     }
+//     else if ( b == c &&  b < a  ){
+//         pp.innerHTML =  b + " va " + c + d
+//     }
+
+
+// }
+// button.onclick = hisob
+
+///////////////////////////////////////////////   /////////////////////////// Kichik son
 
 
 
 
-let button = document.getElementById('button')
-let pp = document.getElementById('pp')
 
 
-function hisob() {
-    let input1 = document.getElementById('input1').value
-    let input2 = document.getElementById('input2').value
-    let input3 = document.getElementById('input3').value
-    let a = input1
-    let b = input2
-    let c = input3
-    let d = "  soni eng kichigi"
-
-    if( a == b && b == c && c == a){
-        pp.innerHTML = " Bu sonlar o'zaro teng"
-    }
-    else if ( a < b && a < c){
-        pp.innerHTML = a +  d
-    }
-    else if (b < a && b < c ){
-        pp.innerHTML = b + d
-    }
-    else if ( c < a && c < b){
-        pp.innerHTML = c + d
-    }
-    else if ( a == b && b < c || a == b && b > c )
-    pp.innerHTML = a + " va " + b +" tang" + c 
+// let button = document.getElementById('button')
+// let pp = document.getElementById('pp')
 
 
-}
-button.onclick = hisob
+// function hisob() {
+//     let input1 = +document.getElementById('input1').value
+//     let input2 = +document.getElementById('input2').value
+//     let input3 = +document.getElementById('input3').value
+//     let a = input1
+//     let b = input2
+//     let c = input3
+//     let d = "  soni orta son"
+
+//  if( a > b && a < c   || a < b && a > c ){
+//     pp.innerHTML = a + d
+//  }
+//  else if ( b > a && b < c || b < a && b > c ){
+//      pp.innerHTML = b + d
+//  }
+
+//  else if ( c > a && c < b || c < a && c > b ){
+//     pp.innerHTML = c + d
+// }
+// else{
+//     pp.innerHTML = " siz ikki yoki uchta birxil son kiritdingiz"
+// }
+// }
+// button.onclick = hisob
+///////////////////////////////////////////////   /////////////////////////// o'rta son
