caf34f3c285a7bd19461cf60fdd37fa10dd1f506
Your Name <you@example.com>
2024-02-24 17:51:36 +0500

    js

---
+++ 10-dars vazifa/script.js
@@ -0,0 +1,77 @@
+
+
+
+// let button = document.getElementById('button')
+// let pp = document.getElementById('pp')
+
+
+// function hisob() {
+//     let son1 = +document.getElementById('son1').value
+//     let amal = document.getElementById('amal').value
+//     let son2 = +document.getElementById('son2').value
+//     let javob;
+//     switch (amal) {
+//         case "+":
+//             javob = son1 + son2;
+//             break;
+//             case "-":
+//             javob = son1 - son2;
+//             break;
+//             case "*":
+//             javob = son1 * son2;
+//             break;
+//             case "/" :
+//                 javob = son1 / son2;
+//     }
+//      pp.innerHTML = javob
+// }
+// button.onclick = hisob
+
+
+
+
+// function hisob() {
+//     let son1 = +document.getElementById('son1').value
+//     let amal = document.getElementById('amal').value
+//     let son2 = +document.getElementById('son2').value
+//     let javob;
+//     let natija;
+//     switch (amal) {
+//         case "+":
+//             javob = son1 + son2;
+//             break;
+//             case "-":
+//             javob = son1 - son2;
+//             break;
+//             case "*":
+//             javob = son1 * son2;
+//             break;
+//             case "/" :
+//                 javob = son1 / son2;
+//     }
+    
+//     natija = son1 + amal + son2 + "=" ;
+//     pp.innerHTML = natija + javob;
+// }
+// button.onclick = hisob
+
+
+
+
+
+
+// let pp = document.getElementById('pp')
+// let button = document.getElementById('button')
+
+// function hisob (){
+//     let input = document.getElementById('hello').value
+//     let input2 = document.getElementById('input2').value
+
+//     for(let i = 0; i <= input2; i++){
+//        pp.innerHTML += i + " " +  input + "<br>"; 
+//     // console.log(i =+ input);
+//     }
+// }
+// button.onclick = hisob
+
+/////////////////////////////////  Takrorlanishi //////////////////////////
