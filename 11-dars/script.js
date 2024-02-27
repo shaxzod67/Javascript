6191be2572b1bc076d0a2d4bbca113d4c5182823
Your Name <you@example.com>
2024-02-27 17:48:59 +0500

    new17

--- 11-dars/script.js
+++ 11-dars/script.js
@@ -2,6 +2,26 @@ let button = document.getElementById('button');
 let p = document.getElementById('pp');
 
 
-function myJS(){
+function myJS(a,b){
+    let amal = document.getElementById('amal').value
+    switch(amal){
+        case "+":
+            return a + b;
+             case "-":
+             return a - b;
+             case "*":
+             return a * b;
+             case "/":
+             return a / b;
+             case "**":
+             return a ** b;
+        }
+        
+    }
     
+    function hisob (){
+        let y = +document.getElementById('son2').value
+        let x = +document.getElementById('son1').value
+    p.innerHTML = myJS(x,y);
 }
+button.onclick = hisob
