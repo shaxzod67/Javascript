796a30aaf8c2fca8e17ab041aaeb06cf71ffe80d
Your Name <you@example.com>
2024-02-29 15:49:10 +0500

    12dars

---
+++ 12-dars/script.js
@@ -0,0 +1,41 @@
+// 1 masala
+// let pp = document.querySelector('#qiymat');
+// let button = document.querySelector('#button');
+
+
+
+// function hisob(a, b, c) {
+//     return a + b + c;
+// }
+// function javob() {
+//     let bir = +document.querySelector('#bir').value
+//     let ikki = +document.querySelector('#ikki').value
+//     let uch = +document.querySelector('#uch').value
+
+
+//    pp.innerHTML = (hisob(bir,ikki,uch));
+// }
+
+
+/////////////////////////////////////////////////////////////// 2-masala
+// let pp = document.querySelector('#qiymat');
+// let button = document.querySelector('#button');
+
+
+// function hisob() {
+//     let bir = +document.querySelector('#bir').value
+//     let ikki = +document.querySelector('#ikki').value
+
+//     if (bir > ikki) {
+//         pp.innerHTML = bir + " Katta son a"
+//     } else if (ikki > bir) {
+//         pp.innerHTML = ikki + " Katta son b"
+//     }
+// }
+
+/////////////////////////////////////////////////////////////// 3-masala
+// let a = '*';
+// for(let i = 1; i <= 5; i++){
+//      i += (a);
+//      console.log(i);
+// }
