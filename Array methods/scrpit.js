ba090ebb29b6c23129be660d52e9b39c4613a192
sardor1411 <shahnozka1104@gmail.com>
2024-03-28 15:22:59 +0500

    string methods

---
+++ Array methods/scrpit.js
@@ -0,0 +1,35 @@
+// let son = ['Olimjonov' , 'Shahzod',23, 'Usmon',51];
+// let total = son.slice(2 , 5);
+// console.log(total);
+//  slice "dan" => "gacha" bolin ishledi yani index raqami bo'yicha.
+////// slice ///////
+
+// let son = ['Olimjonov' , 'Shahzod',23, 'Usmon',51];
+// let total = son.reverse();
+// console.log(total);
+// console.log(total[1]);
+
+// Teskari qiladi va index raqam ham ozgaradi
+
+////// reverse ///////
+
+
+// let son = ['Olimjonov', 'Shahzod', 23, 'Usmon', 51];
+// let total = son.join(' ');
+// console.log(total);
+
+// Array malumotni string qiladi va ('') => bu qavs ichida ishora yoziladi va bunishorani quyib chiqarib beradi
+////// join ///////
+
+
+// let son = ['Olimjonov', 'Shahzod', 23, 'Usmon', 51];
+// let total = son.splice( 1, 3);
+// console.log(total);
+
+// splice va slice oxshash farqi splice oxirgi indexni oladi
+
+// let son = ['Olimjonov', 'Shahzod', 23, 'Usmon', 51];
+// let total = son.forEach();
+// console.log(total);
+
+// ???
