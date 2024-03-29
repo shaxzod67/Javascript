304324d9f56462be12bc74253ff0656ae97fc560
sardor1411 <shahnozka1104@gmail.com>
2024-03-29 13:31:16 +0500

    new array methods

---
+++ Array methods/main.js
@@ -0,0 +1,44 @@
+ 
+//  1 - Join va tostring bu ikki metod massivlarni stringga o'zgartirib beardi.
+// let nom = ['Shaxzod' , 'Olimjonov' , 'Usmon' , '23'];
+
+// let nomString = nom.join(' ')
+// console.log(nomString);
+
+////////////////////////////////////concat///////////////////////////////////
+
+//  2 - concat Bu metod 2 va undan ortiq  massivlarni bir biriga qo'shib beradi.
+
+// let nom  = ['shaxzod' , 'Olimjonov' , 'Ozod ogli' , 23];
+// let nom1  = ['Uchqun' , 'Xurramov' , 'Tolmas ogli' , 21];
+// let nom3  = ['oybek' , 'Hasanov' , 'Otabek ogli' , 24];
+
+// let nomPlus = nom.concat(nom1 ,nom3);
+// console.log(nomPlus);
+
+//////////////////////////////////////splice/////////////////////////////////
+
+//  3 - splice bu massivni index raqami oraligini belgilaydi va shu ichki oraliqni kesib tashab tashqi oraliqni chiqaradi va unga ozimz yana array qosha olamiz.
+// let nom  = ['shaxzod' , 'Olimjonov' , 'Ozod ogli' , 23];
+// nom.splice(1 , 2);
+// console.log(nom);
+
+//////////////////////////////////////slice/////////////////////////////////
+
+
+//  4 - slice bu massivni index raqami oraligini belgilaydi va shu ichki oraliqni kesib oladi tashqi oraliqni ochiradi va ichgi oraliqni chiqaradi n dan m gacha bolib ishlaydi unga ozimiz yana array qosha olmaymiz.
+// let nom  = ['shaxzod' , 'Olimjonov' , 'Ozod ogli' , 23];
+// let slice = nom.slice(0 ,2 , 'hw');
+// console.log(slice);
+
+//////////////////////////////////////indexOf/////////////////////////////////
+
+// 5 - bu indexOf array ichidagi malumotni yozamiz va u bizga u boshidan izlab nechanchi indexligini chiqaradi 
+// 6 - bu lastindexOf array ichidagi malumotni yozamiz va u oxiridan izlab bizga u nechanchi indexligini chiqaradi 
+
+// let nom  = ['shaxzod' , 'Olimjonov' , 'Ozod ogli' , 23];
+// let index = nom.lastIndexOf(23);
+// console.log(index);
+
+//////////////////////////////////////indexOf/////////////////////////////////
+
