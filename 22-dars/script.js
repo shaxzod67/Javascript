68d8626414f5a004ccad593300b9badbe4db95a8
sardor1411 <shahnozka1104@gmail.com>
2024-04-01 13:23:04 +0500

    crud

---
+++ 22-dars/script.js
@@ -0,0 +1,54 @@
+let pp = document.querySelector('#pp');
+let name = document.querySelector('#name');
+let date = document.querySelector('#date');
+let ball = document.querySelector('#ball');
+let btn = document.querySelector('#btn');
+let data = document.querySelector('#data');
+let form = document.querySelector('#form');
+let del = document.querySelector('#bxs-trash');
+
+const mall = [];
+form.addEventListener("submit", function (qaytarish) {
+    qaytarish.preventDefault();
+    console.log("Hello");
+    tekshirish();
+});
+
+function tekshirish() {
+    if (name.value == "" || date.value == "" || ball.value == "") {
+        pp.style.color = "red";
+        return pp.innerHTML = "Malumotni to'liq kiriting"
+    }
+    else {
+        pp.style.color = "green";
+        pp.innerHTML = "Malumot yuborildi"
+    }
+    ozgarish();
+}
+
+function ozgarish() {
+    mall.push(
+        {
+            name: name.value,
+            date:date.value,
+            ball:ball.value,
+        }
+    )
+    console.log(mall);
+    korsatish();
+}
+
+function korsatish(){
+    data.innerHTML = "";
+    mall.map(function(ozgrvc , index){
+        data.innerHTML += `
+        <td>${index + 1}</td>
+        <td>${ozgrvc.name}</td>
+        <td>${ozgrvc.date}</td>
+        <td>${ozgrvc.ball}</td>
+        <td><i class='bx bxs-edit'></i></td>
+        <td><i class='bx bxs-trash'></i></td>
+        `
+    })
+}
+ 
