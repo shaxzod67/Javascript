4ed6091894a0e15e47ffb7c6ee4b01abeabb490c
sardor1411 <shahnozka1104@gmail.com>
2024-04-03 14:34:40 +0500

    new

--- 22-dars/script.js
+++ 22-dars/script.js
@@ -1,54 +1,113 @@
+// let pp = document.querySelector('#pp');
+// let name = document.querySelector('#name');
+// let date = document.querySelector('#date');
+// let ball = document.querySelector('#ball');
+// let btn = document.querySelector('#btn');
+// let data = document.querySelector('#data');
+// let form = document.querySelector('#form');
+// let del = document.querySelector('#bxs-trash');
+
+// const mall = [];
+// form.addEventListener("submit", function (qaytarish) {
+//     qaytarish.preventDefault();
+//     console.log("Hello");
+//     tekshirish();
+// });
+
+// function tekshirish() {
+//     if (name.value == "" || date.value == "" || ball.value == "") {
+//         pp.style.color = "red";
+//         return pp.innerHTML = "Malumotni to'liq kiriting"
+//     }
+//     else {
+//         pp.style.color = "green";
+//         pp.innerHTML = "Malumot yuborildi"
+//     }
+//     ozgarish();
+// }
+
+// function ozgarish() {
+//     mall.push(
+//         {
+//             name: name.value,
+//             date:date.value,
+//             ball:ball.value,
+//         }
+//     )
+//     console.log(mall);
+//     korsatish();
+// }
+
+// function korsatish(){
+//     data.innerHTML = "";
+//     mall.map(function(ozgrvc , index){
+//         data.innerHTML += `
+//         <td>${index + 1}</td>
+//         <td>${ozgrvc.name}</td>
+//         <td>${ozgrvc.date}</td>
+//         <td>${ozgrvc.ball}</td>
+//         <td><i class='bx bxs-edit'></i></td>
+//         <td><i class='bx bxs-trash'></i></td>
+//         `
+//     })
+// }
+
+
+
 let pp = document.querySelector('#pp');
 let name = document.querySelector('#name');
 let date = document.querySelector('#date');
 let ball = document.querySelector('#ball');
 let btn = document.querySelector('#btn');
-let data = document.querySelector('#data');
 let form = document.querySelector('#form');
-let del = document.querySelector('#bxs-trash');
+let data = document.querySelector('#data');
+
 
 const mall = [];
-form.addEventListener("submit", function (qaytarish) {
-    qaytarish.preventDefault();
-    console.log("Hello");
-    tekshirish();
+form.addEventListener('submit', function (qayt) {
+    qayt.preventDefault();
+    tek();
+    console.log("hello");
 });
 
-function tekshirish() {
+function tek() {
     if (name.value == "" || date.value == "" || ball.value == "") {
         pp.style.color = "red";
-        return pp.innerHTML = "Malumotni to'liq kiriting"
+        return pp.innerHTML = "Malumotni toliq kiriting "
     }
     else {
         pp.style.color = "green";
-        pp.innerHTML = "Malumot yuborildi"
+        pp.innerHTML = "Malumot kiritildi";
     }
-    ozgarish();
+    elon();
 }
-
-function ozgarish() {
+function elon() {
     mall.push(
         {
             name: name.value,
-            date:date.value,
-            ball:ball.value,
+            date: date.value,
+            ball: ball.value,
         }
     )
-    console.log(mall);
-    korsatish();
+    oqish();
 }
 
-function korsatish(){
-    data.innerHTML = "";
-    mall.map(function(ozgrvc , index){
+function oqish() {
+    data.innerHTML = '';
+    mall.map(function (value, index) {
         data.innerHTML += `
-        <td>${index + 1}</td>
-        <td>${ozgrvc.name}</td>
-        <td>${ozgrvc.date}</td>
-        <td>${ozgrvc.ball}</td>
-        <td><i class='bx bxs-edit'></i></td>
-        <td><i class='bx bxs-trash'></i></td>
-        `
+     <td>${index + 1}</td>
+     <td>${value.name}</td>
+     <td>${value.date}</td>
+     <td>${value.ball}</td>
+     <td><i class='bx bxs-edit'></i></td>
+     <td><i class='bx bxs-trash'></i></td>
+    `
     })
+    document.getElementById("name").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("ball").value = "";
+
 }
- 
+
+
